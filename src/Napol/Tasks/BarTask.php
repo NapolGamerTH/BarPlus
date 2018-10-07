d5bc37f51e14e2aa718bd6e7c1ee4caa3f52cb9e
NapolGamer TH <knightnapol15@gmail.com>
2018-10-07 12:17:16 +0700

    Update BarTask.php

--- src/Napol/Tasks/BarTask.php
+++ src/Napol/Tasks/BarTask.php
@@ -3,18 +3,17 @@
 namespace Napol\Tasks;
 
 use pocketmine\Server;
-use pocketmine\scheduler\PluginTask;
+use pocketmine\scheduler\Task;
 use Napol\BarPlus;
 use pocketmine\plugin\Plugin;
 
-class BarTask extends PluginTask{
+class BarTask extends Task{
 
     public function __construct(BarPlus $plugin){
         $this->plugin = $plugin;
-        parent::__construct($plugin);
     }
 
-    public function onRun($tick){
+    public function onRun(int $currentTick){
 		$this->plugin->onBar();
     }
 
